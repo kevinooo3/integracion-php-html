564c7071b800c325110bf79bfed064d62d57b05a
kevinooox <kevinooo_18@hotmail.com>
2023-03-27 08:57:27 -0500

    subi el curso de composer-php

---
+++ composer-php/creacion-manual-composer/src/Format.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Text;
+class Format{
+
+    public static function mayuscula($cadena){
+
+        return strtoupper($cadena);
+    }
+}
---
+++ composer-php/creacion-manual-composer/src/helpers.php
@@ -0,0 +1,5 @@
+<?php
+
+    function convertMayuscula($valor){
+        return Text\Format::mayuscula($valor);
+    }
